ef9c8782ce674fd73c4353829c0c734881f179c6
JuliMiguel <julliana.miguel@gmail.com>
2021-09-06 02:07:42 -0300

    atualizações2

--- src/main/java/com/dio/personapi/Repositories/PhoneRepository.java
+++
@@ -1,7 +0,0 @@
-package com.dio.personapi.Repositories;
-
-import com.dio.personapi.Entities.Phone;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PhoneRepository extends JpaRepository<Phone, Long> {
-}
